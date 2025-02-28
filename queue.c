201fe14e26410abf8d5dc154b36d91927fd41cc6
dingsen-Greenhorn <q36134289@gs.ncku.edu.tw>
2025-02-28 21:47:37 +0800

    Second commit
    finish  q_remove_tail and head
    q_size,q_delete_mid and dup ,q_reverse set
    The function of Remove,delete,and swap
    
    Change-Id: I2540b2db61d5365ee40d00f00390f76826a6dbc5

--- queue.c
+++ queue.c
@@ -86,3 +86,167 @@ bool q_insert_tail(struct list_head *head, char *s)
     list_add_tail(&new_content->list, head);
     return true;
 }
+
+/* Remove an element at head of queue */
+element_t *q_remove_head(struct list_head *head, char *sp, size_t bufsize)
+{
+    if (!head || list_empty(head)) {
+        return NULL;
+    }
+
+    element_t *first = list_first_entry(head, element_t, list);
+    list_del(&first->list);
+
+    if (sp && bufsize > 0) {
+        strncpy(sp, first->value, bufsize - 1);
+        sp[bufsize - 1] = '\0';
+    }
+
+    return first;
+}
+
+
+/* Remove an element at tail of queue */
+element_t *q_remove_tail(struct list_head *head, char *sp, size_t bufsize)
+{
+    if (!head || list_empty(head)) {
+        return NULL;
+    }
+
+    element_t *tail = list_last_entry(head, element_t, list);
+    list_del(&tail->list);
+
+    if (sp && bufsize > 0) {
+        strncpy(sp, tail->value, bufsize - 1);
+        sp[bufsize - 1] = '\0';
+    }
+
+    return tail;
+}
+
+
+/* Return number of elements in queue */
+int q_size(struct list_head *head)
+{
+    if (!head)
+        return 0;
+
+    int len = 0;
+    struct list_head *lenptr;
+
+    list_for_each (lenptr, head)
+        len = len + 1;
+    return len;
+}
+
+/* Delete the middle node in queue */
+bool q_delete_mid(struct list_head *head)
+{
+    // https://leetcode.com/problems/delete-the-middle-node-of-a-linked-list/
+    if (!head || list_empty(head))
+        return false;
+
+    struct list_head *fast = head->next, *slow = head->next;
+
+    while (fast != head && fast->next != head) {
+        fast = fast->next->next;
+        slow = slow->next;
+    }
+    element_t *mid = list_entry(slow, element_t, list);
+    list_del(slow);
+    free(mid->value);
+    free(mid);
+    return true;
+}
+
+
+/* Delete all nodes that have duplicate string */
+bool q_delete_dup(struct list_head *head)
+{
+    if (!head || list_empty(head)) {
+        return false;
+    }
+
+    struct list_head *node, *safe;
+    element_t *prev = NULL, *curr = NULL;
+    bool has_dup = false;
+
+    list_for_each_safe (node, safe, head) {
+        curr = list_entry(node, element_t, list);
+
+        if (prev && !strcmp(prev->value, curr->value)) {
+            list_del(node);
+            free(curr->value);
+            free(curr);
+            has_dup = true;
+        } else {
+            if (has_dup) {
+                list_del(&prev->list);
+                free(prev->value);
+                free(prev);
+                has_dup = false;
+            }
+            prev = curr;
+        }
+    }
+    if (has_dup && prev) {
+        list_del(&prev->list);
+        free(prev->value);
+        free(prev);
+    }
+
+    return true;
+}
+
+/* Swap every two adjacent nodes */
+void q_swap(struct list_head *head)
+{
+    // https://leetcode.com/problems/swap-nodes-in-pairs/
+    q_reverseK(head, 2);
+}
+
+/* Reverse elements in queue */
+/* Reverse elements in queue */
+void q_reverse(struct list_head *head)
+{
+    if (!head || list_empty(head)) {
+        return;
+    }
+    struct list_head *node, *safe;
+    list_for_each_safe (node, safe, head) {
+        node->next = node->prev;
+        node->prev = safe;
+    }
+    node->next = node->prev;
+    node->prev = safe;
+    return;
+}
+
+/* Reverse the nodes of the list k at a time */
+void q_reverseK(struct list_head *head, int k)
+{
+    // https://leetcode.com/problems/reverse-nodes-in-k-group/
+    if (!head) {
+        return;
+    }
+
+    int split_time = q_size(head) / k;
+    struct list_head *tail;
+
+    LIST_HEAD(tmp);
+    LIST_HEAD(new_head);
+
+    for (int i = 0; i < split_time; i = i + 1) {
+        int j = 0;
+        list_for_each (tail, head) {
+            if (j >= k) {
+                break;
+            }
+            j = j + 1;
+        }
+        list_cut_position(&tmp, head, tail->prev);
+        q_reverse(&tmp);
+        list_splice_tail_init(&tmp, &new_head);
+    }
+    list_splice_init(&new_head, head);
+}
